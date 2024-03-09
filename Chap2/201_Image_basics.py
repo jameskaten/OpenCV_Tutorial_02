0a58eb0df5fedab7f0cdb97667365256b135be1d
jameskaten <jameskaten@gmail.com>
2024-03-09 06:38:50 -0800

    Image read

---
+++ Chap2/201_Image_basics.py
@@ -0,0 +1,13 @@
+import cv2
+
+# Image Loading
+
+img_file = "Twins.jpg"
+img = cv2.imread(img_file)
+
+if img is not None:
+    cv2.imshow('IMG', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+else:
+    print('No image file.')
