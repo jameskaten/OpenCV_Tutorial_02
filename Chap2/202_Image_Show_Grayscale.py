b171d1e6485f876ad7056cb72d7586e3be9ecf4c
jameskaten <jameskaten@gmail.com>
2024-03-11 21:46:56 -0700

    Add files

---
+++ Chap2/202_Image_Show_Grayscale.py
@@ -0,0 +1,13 @@
+import cv2
+
+# Image Loading
+
+img_file = "../Chris02.jpg"
+img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
+
+if img is not None:
+    cv2.imshow('IMG', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+else:
+    print('No image file.')
---
+++ Chap2/203_Image_Write.py
@@ -0,0 +1,12 @@
+import cv2
+
+# Image Loading
+
+img_file = "../Chris02.jpg"
+save_file = "Chris02_saved.jpg" \
+            ""
+img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
+cv2.imshow('IMG', img)
+cv2.imwrite(save_file, img)
+cv2.waitKey()
+cv2.destroyAllWindows()
