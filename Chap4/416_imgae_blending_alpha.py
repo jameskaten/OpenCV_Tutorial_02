a1e4e4abfa6ebd37d5af71260022640e291f6388
jameskaten <jameskaten@gmail.com>
2024-03-31 22:16:53 -0700

    Add a new file

---
+++ Chap4/416_imgae_blending_alpha.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+alpha = 0.5
+
+img1 = cv2.imread('../img/wing_wall.jpg')
+img2 = cv2.imread('../img/yate.jpg')
+
+# alpha blending
+blended = img1 * alpha + img2 * (1 - alpha)
+blended = blended.astype(np.uint8)
+cv2.imshow('img1 * alpha + img2 * (1 - alpha)', blended)
+
+# addWeighted() function alpha blending
+dst = cv2.addWeighted(img1, alpha, img2, (1-alpha), 0)
+cv2.imshow('cv2.addWeighted', dst)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
