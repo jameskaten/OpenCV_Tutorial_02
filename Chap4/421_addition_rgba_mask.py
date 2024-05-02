7b4898953ae1523d8e4281bbf19d73b72404230d
jameskaten <jameskaten@gmail.com>
2024-05-01 22:26:09 -0700

    Add a new file

---
+++ Chap4/421_addition_rgba_mask.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+# image read for merge
+img_fg = cv2.imread('../img/opencv_logo.png', cv2.IMREAD_UNCHANGED)
+img_bg = cv2.imread('../img/Angela01.jpg')
+
+# Alpha channel mask and inverse mask generation
+_, mask = cv2.threshold(img_fg[:,:,3], 1, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+
+# Cut ROI
+img_fg = cv2.cvtColor(img_fg, cv2.COLOR_BGRA2BGR)
+h, w = img_fg.shape[:2]
+roi = img_bg[10:10+h, 10:10+w]
+
+# cut with mask
+masked_fg = cv2.bitwise_and(img_fg, img_fg, mask=mask)
+masked_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+
+# image combine
+added = masked_fg + masked_bg
+img_bg[10:10+h, 10:10+w] = added
+
+cv2.imshow('mask', mask)
+cv2.imshow('mask_inv', mask_inv)
+cv2.imshow('masked_fg', masked_fg)
+cv2.imshow('masked_bg', masked_bg)
+cv2.imshow('added', added)
+cv2.imshow('result', img_bg)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
