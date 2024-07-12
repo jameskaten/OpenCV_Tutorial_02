37e24d5cde5d5eefbe8821ade19a0a981242feec
jameskaten <jameskaten@gmail.com>
2024-07-11 21:47:07 -0700

    Add a Laplacian filter

---
+++ Chap6/611_edge_laplacian.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../img/sudoku.png')
+
+# Laplacian filter
+edge = cv2.Laplacian(img, -1)
+
+merged = np.hstack((img, edge))
+cv2.imshow('Laplacian', merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
