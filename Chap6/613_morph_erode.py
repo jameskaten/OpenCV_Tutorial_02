a79dc90c4561a87236dc8e344250ed1d32c69b8b
jameskaten <jameskaten@gmail.com>
2024-07-14 19:07:00 -0700

    Add a morph_erode

---
+++ Chap6/613_morph_erode.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../img/morph_dot.png')
+
+# kerner, square
+k = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+# erode
+erosion = cv2.erode(img, k)
+
+merged = np.hstack((img, erosion))
+cv2.imshow('Erode', merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
