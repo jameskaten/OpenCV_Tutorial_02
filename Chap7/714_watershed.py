6ee24542865a490ff674716a2360da8f689c526b
jameskaten <jameskaten@gmail.com>
2024-08-12 22:42:39 -0700

    Add a new file

---
+++ Chap7/714_watershed.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../img/taekwonv1.jpg')
+rows, cols = img.shape[:2]
+img_draw = img.copy()
+
+marker = np.zeros((rows, cols), np.int32)
+markerId = 1
+colors = []
+isDragging = False
+
+def onMouse(event, x, y, flags, param):
+    global img_draw, marker, markerId, isDragging
+    if event == cv2.EVENT_LBUTTONDOWN:
+        isDragging = True
+        colors.append((markerId, img[y,x]))
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if isDragging:
+            marker[y,x] = markerId
+            # red dot
+            cv2.circle(img_draw, (x,y), 3, (0,0,255), -1)
+            cv2.imshow('watershed', img_draw)
+    elif event == cv2.EVENT_LBUTTONUP:
+        if isDragging:
+            isDragging = False
+            # 다음 마커 선택을 위해 마커 아이디 증가
+            markerId += 1
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        cv2.watershed(img, marker)
+        # 마커에 -1로 표시된 경계를 초록색으로 표시
+        img_draw[marker == -1] = (0,255,0)
+        for mid, color in colors:
+            img_draw[marker==mid] = color
+        cv2.imshow('watershed', img_draw)
+
+cv2.imshow('watershed', img)
+cv2.setMouseCallback('watershed', onMouse)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
