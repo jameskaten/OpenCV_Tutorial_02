fe4af480e268d83d2b270b17838cfa9c42965f14
jameskaten <jameskaten@gmail.com>
2024-12-16 22:24:02 -0800

    update comments

--- Chap2/204_Video_file_reading.py
+++ Chap2/204_Video_file_reading.py
@@ -1,12 +1,12 @@
 import cv2
 
-video_file = "../Angela.mp4"
+video_file = "../img/Angela.mp4"
 
-cap = cv2.VideoCapture(video_file)
+cap = cv2.VideoCapture(video_file)          # video capture object
 if cap.isOpened():      # Initialization
     while True:
-        ret, img = cap.read()   # read next frame
-        if ret:
+        ret, img = cap.read()   # read next frame, ret: reading True/False
+        if ret:                 # reading successful? 
             cv2.imshow(video_file, img)
             cv2.waitKey(25)
         else:
