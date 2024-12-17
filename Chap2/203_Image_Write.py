305cc9ef6e51af4bfc493babbe5e61a41c3ba583
jameskaten <jameskaten@gmail.com>
2024-12-16 22:12:33 -0800

    Update comments

--- Chap2/203_Image_Write.py
+++ Chap2/203_Image_Write.py
@@ -3,10 +3,10 @@ import cv2
 # Image Loading
 
 img_file = "../img/Chris02.jpg"
-save_file = "Chris02_saved.jpg" \
-            ""
-img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
+save_file = "Chris02_saved.jpg"
+
+img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)       # Reading Gray scale
 cv2.imshow('IMG', img)
-cv2.imwrite(save_file, img)
+cv2.imwrite(save_file, img)         # Save file
 cv2.waitKey()
 cv2.destroyAllWindows()
