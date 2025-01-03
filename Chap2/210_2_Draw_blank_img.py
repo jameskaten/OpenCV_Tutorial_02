98f82e14d1a30d81173e2a3b019cada486b16430
jameskaten <jameskaten@gmail.com>
2025-01-04 07:42:19 +0900

    Add comments and but fixed

--- Chap2/210_2_Draw_blank_img.py
+++ Chap2/210_2_Draw_blank_img.py
@@ -1,5 +1,12 @@
 import cv2
 import numpy as np
 
+# Create a blank white image
+# np.full parameters:
+#   - shape: (500, 500, 3) means 500x500 pixels with 3 color channels (BGR)
+#   - value: 255 means white (in all channels)
+#   - dtype: np.uint8 means 8-bit unsigned integer (0-255 range)
 img = np.full((500, 500, 3), 255, dtype=np.uint8)
-cv2.imwrite('../img/blank_500.jpg')
+
+# Save the blank white image
+cv2.imwrite('../img/blank_500.jpg', img)
